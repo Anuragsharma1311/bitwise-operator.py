fd8a381d6061ac1d53f1b26f29fc68fb79667f4a
Anuragsharma1311 <124031761+Anuragsharma1311@users.noreply.github.com>
2023-02-03 16:52:12 +0530

    Add files via upload

---
+++ bitwise operator.py
@@ -0,0 +1,17 @@
+x=10
+print(x, bin(x))
+
+y=15
+print(y, bin(y))
+
+print(x&y, bin (x&y))
+
+print(x|y, bin(x|y))
+
+print(x^y, bin(x^y))
+
+print(bin(39))
+
+print((10))
+print((20,[]))
+print((30))
